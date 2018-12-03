48c70a87f1b071bc96b0e18be9bd09d4db414183
Valentin <ghury1@gmail.com>
2018-12-03 15:32:33 +0100

    aggiungo tutti i file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
