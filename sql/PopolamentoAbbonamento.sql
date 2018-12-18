f091a9be7da4ab9c7d78c8823376c0ddf5821698
daniel mirel <daniel.mirel88@yahoo.com>
2018-12-18 14:02:39 +0100

    aggiunta PopolamentoAbbonamento

---
+++ sql/PopolamentoAbbonamento.sql
@@ -0,0 +1,4 @@
+INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione) VALUES ('1','Mensile','50','');
+INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione) VALUES ('2','Trimestrale','120','');
+INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione) VALUES ('3','Semestrale','250','');
+INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione) VALUES ('4','Annuale','360','');
