b55bf69a0cd205dadf6db4b8c237b75027725600
Irina <irina_030@protonmail.com>
2019-07-24 00:41:23 +0200

    update registrazione form

--- html/pages/registrazione.php
+++ html/pages/registrazione.php
@@ -12,7 +12,7 @@
 <!-- ------------------------------------------------------ -->
 <div>
 	<div id="contenuto">
-		<div id="header">
+		<div id="intestazione">
 			<h1> Unisciti a AIMFit </h1>
 			<p> Offre soluzioni d’allenamento in grado di rispondere ad ogni esigenza! </p>
 		</div>
@@ -23,124 +23,132 @@
 					<p>I campi con (*) sono obbligatori.</p>
 					<fieldset>
                         <legend>Account</legend>
-                        <div class="container formfield">
-                            <div class="col-4 col-xl-2">
-                                <label for="nome">Email (*)</label>
-                                <input tabindex="*tabindexemail*" type="text" name="email" id="email" size="12" maxlength="20" *disabled* />
+                        <div>
+                            <div class="required">
+                                <label for="email" class="input_label ">Email</label>
+                                <input tabindex="*tabindexemail*" type="text" name="email" id="email" maxlength="20" *disabled* />
                             </div>
-                            <p class="col-4 col-xl-2">Inserire l'<span xml:lang="en">email</span> nel formato casella@dominio.</p>
-                            *erroremail*
+                            <p class="form_note">Inserire l'<span xml:lang="en">email</span> nel formato casella@dominio.</p>
+                            <p hidden="true">*erroremail*</p>
                         </div>
-                        <div class="container formfield">
-                            <div class="col-4 col-xl-2">
-                                <label for="password">Password (*)</label>
-                                <input tabindex="*tabindexpassword*" type="password" name="password" id="password" size="12" maxlength="20" *disabled*/>
+                        <div>
+                            <div class="required">
+                                <label for="password" class="input_label ">Password</label>
+                                <input tabindex="*tabindexpassword*" type="password" name="password" id="password"  maxlength="20" *disabled*/>
                             </div>
-                            <p class="col-4 col-xl-2">Inserire la password scelta.</p>
-                            *errorpassword*
-                            <div class="col-4 col-xl-2">
-                                <label for="confermapassword">Conferma password (*)</label>
-                                <input tabindex="*tabindexconfermapassword*" type="password" name="confermapassword" id="confermapassword" size="12" maxlength="20" *disabled*/>
+                            <p class="form_note">Inserire la password scelta.</p>
+                            
+                            <p hidden="true">*errorpassword*</p>
+                            <div class="required">
+                                <label for="confermapassword" class="input_label ">Conferma password</label>
+                                <input tabindex="*tabindexconfermapassword*" type="password" name="confermapassword" id="confermapassword"  maxlength="20" *disabled*/>
                             </div>
-                            <p class="col-4 col-xl-2">Inserire nuovamente la password scelta.</p>
-                            *errorconfermapassword*
+                            <p class="form_note">Inserire nuovamente la password scelta.</p>
+                            
+                            <p hidden="true">*errorconfermapassword*</p>
                         </div>
                     </fieldset>
                     <fieldset>
                         <legend>Dati personali</legend>
-                        <div class="container formfield">
-                            <div class="col-4 col-xl-2">
-                                <label for="nome">Nome (*)</label>
-                                <input tabindex="*tabindexnome*" type="text" name="nome" id="nome" size="12" maxlength="20" *disabled* />
+                        <div>
+                            <div class="required">
+                                <label for="nome" class="input_label ">Nome</label>
+                                <input tabindex="*tabindexnome*" type="text" name="nome" id="nome"  maxlength="20" *disabled* />
                             </div>
-                            <p class="col-4 col-xl-2">Inserire il nome senza spazi e con sole lettere alfabetiche.</p>
-                            *errorenome*
+                            <p class="form_note">Inserire il nome senza spazi e con sole lettere alfabetiche.</p>
+                            
+                            <p hidden="true">*errorenome*</p>
                         </div>
-                        <div class="container formfield">
-                            <div class="col-4 col-xl-2">
-                                <label for="cognome">Cognome (*)</label>
-                                <input tabindex="*tabindexcognome*" type="text" name="cognome" id="cognome" size="12" maxlength="20" *disabled*/>
+                        <div>
+                            <div class="required">
+                                <label for="cognome" class="input_label required ">Cognome</label>
+                                <input tabindex="*tabindexcognome*" type="text" name="cognome" id="cognome" maxlength="20" *disabled*/>
                             </div>
-                            <p class="col-4 col-xl-2">Inserire il cognome senza spazi e con sole lettere alfabetiche.</p>
-                            *errorcognome*
+                            <p class="form_note">Inserire il cognome senza spazi e con sole lettere alfabetiche.</p>
+                            
+                            <p hidden="true">*errorcognome*</p>
                         </div>
-                        <div class="container formfield">
-                            <div class="col-4 container">
-                                <div class="treColonne center">
-                                    <label for="giornonascita">Giorno (*)</label>
+                        <div>
+                            <div>
+                                <div>
+                                    <label for="giornonascita " class="input_label ">Giorno</label>
                                     <select tabindex="*tabindexgiornonascita*" name="giornonascita" id="giornonascita" *disabled*>
-                                        *giornonascita*
+                                        
+                                        <p hidden="true">*giornonascita*</p>
                                     </select>
                                 </div>
-                                <div class="treColonne center">
-                                    <label for="mesenascita">Mese (*)</label>
+                                <div>
+                                    <label for="mesenascita" class="input_label ">Mese</label>
                                     <select tabindex="*tabindexmesenascita*" name="mesenascita" id="mesenascita" *disabled*>
-                                        *mesenascita*
+                                        
+                                        <p hidden="true">*mesenascita*</p>
                                     </select>
 
                                 </div>
-                                <div class="treColonne center">
-                                    <label for="annonascita">Anno (*)</label>
+                                <div >
+                                    <label for="annonascita" class="input_label ">Anno</label>
                                     <select tabindex="*tabindexannonascita*" name="annonascita" id="annonascita" *disabled*>
-                                        *annonascita*
+                                        
+                                        <p hidden="true">*annonascita*</p>
                                     </select>
 
                                 </div>
                             </div>
-                            *errordatanascita*
+                            
+                            <p hidden="true">*errordatanascita*</p>
                         </div>
-                        <div class="container formfield">
-                            <div class="col-4 col-xl-2">
-                                <label for="email"><span xml:lang="en">Codice fiscale</span> (*)</label>
-                                <input tabindex="*tabindexcodicf*" type="text" name="cf" id="cf" size="23" maxlength="50" *disabled*/>
+                        <div>
+                            <div class="required">
+                                <label for="cf" class="input_label ">Codice fiscale</label>
+                                <input tabindex="*tabindexcodicf*" type="text" name="cf" id="cf"  maxlength="50" *disabled*/>
                             </div>
-							<p class="col-4 col-xl-2">Inserire il codice fiscale.</p>
-                            *erroremail*
+							<p class="form_note">Inserire il codice fiscale.</p>
+                            <p hidden="true">*errorecf*</p>
                         </div>
-                        <div class="container formfield">
-                            <div class="col-4 col-xl-2">
-                                <label for="telefono">Telefono (*)</label>
-                                <input tabindex="*tabindextelefono*" type="text" name="telefono" id="telefono" size="15" maxlength="15" *disabled*/>
+                        <div>
+                            <div>
+                                <label for="telefono" class="input_label">Telefono</label>
+                                <input tabindex="*tabindextelefono*" type="text" name="telefono" id="telefono" maxlength="15" *disabled*/>
                             </div>
-                            <p class="col-4 col-xl-2">Inserire il numero di telefono con sole cifre, senza spazi.</p>
-                            *errortelefono*
+                            <p class="form_note">Inserire il numero di telefono con sole cifre, senza spazi.</p>
+                            <p hidden="true">*errortelefono*</p>
                         </div>
                         
                     </fieldset>
                     <fieldset>
                         <legend>Dati relativi all'abitazione</legend>
-                        <div class="container formfield">
-                            <div class="col-4">
-                                <label for="stato">Stato (*)</label>
+                        <div>
+                            <div>
+                                <label for="stato" class="input_label">Stato</label>
                                 <select tabindex="*tabindexstato*" name="stato" id="stato" *disabled*>
                                     *stato*
                                 </select>
                             </div>
-                            *errorstato*
+                            <p hidden="true">*errorstato*</p>
                         </div>
-                        <div class="container formfield">
-                            <div class="col-4 col-xl-2">
-                                <label for="indirizzo">Indirizzo (*)</label>
-                                <input tabindex="*tabindexindirizzo*" type="text" name="indirizzo" id="indirizzo" size="20" *disabled*/>
+                        <div>
+                            <div>
+                                <label for="indirizzo" class="input_label"> Indirizzo</label>
+                                <input tabindex="*tabindexindirizzo*" type="text" name="indirizzo" id="indirizzo"  *disabled*/>
                             </div>
-                            <p class="col-4 col-xl-2">Inserire l'indirizzo con il nome della via e il numero civico.</p>
-                            *errorindirizzo*
+                            <p class="form_note"> Inserire l'indirizzo con il nome della via e il numero civico.</p>
+                            <p hidden="true">*errorindirizzo*</p>
                         </div>
-                        <div class="container formfield">
-                            <div class="col-4 col-xl-2">
-                                <label for="comune">Comune (*)</label>
-                                <input tabindex="*tabindexcomune*" type="text" name="comune" id="comune" size="20" maxlength="20" *disabled*/>
+                        <div>
+                            <div>
+                                <label for="comune" class="input_label">Comune</label>
+                                <input tabindex="*tabindexcomune*" type="text" name="comune" id="comune"  maxlength="20" *disabled*/>
                             </div>
-                            <p class="col-4 col-xl-2">Inserire il comune senza spazi e con sole lettere alfabetiche.</p>
-                            *errorcomune*
+                            <p class="form_note">Inserire il comune senza spazi e con sole lettere alfabetiche.</p>
+                            <p hidden="true">*errorcomune*</p>
                         </div>
-                         <div class="container formfield">
-                            <div class="col-4 col-xl-2">
-                                <label for="provincia">Provincia (*)</label>
-                                <input tabindex="*tabindexprovincia*" type="text" name="provincia" id="provincia" size="20" maxlength="20" *disabled*/>
+                         <div>
+                            <div>
+                                <label for="provincia" class="input_label">Provincia</label>
+                                <input tabindex="*tabindexprovincia*" type="text" name="provincia" id="provincia"  maxlength="20" *disabled*/>
                             </div>
-                            <p class="col-4 col-xl-2">Inserire la provincia senza spazi e con sole lettere alfabetiche.</p>
-                            *errorprovincia*
+                            <p class="form_note">Inserire la provincia senza spazi e con sole lettere alfabetiche.</p>
+                            <p hidden="true">*errorprovincia*</p>
                         </div>
                     </fieldset>
                     <p>Facendo clic su "Crea account" di seguito, accetti i nostri Termini di servizio e l'Informativa sulla privacy. Occasionalmente ti invieremo e-mail relative all'account.</p>
