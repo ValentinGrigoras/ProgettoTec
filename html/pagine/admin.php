c2954eaa15184fa28abb81e632e468a8549cdf42
ValentinGrigoras <ghury1@protonmail.com>
2019-07-26 13:10:00 +0200

    add validator

---
+++ html/pagine/admin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="it" xml:lang="it" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<?php include_once "../templates/head.php"; ?>
+</head>
+
+
+<body>
+        <div class="sidenav" >
+        <ul class ="vertical_menu">
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+        </div>
+        <div class="topnav">  
+                    <ul id="admin_ul">
+                    <li><a href="#login">Login</a></li>
+                    <li><a href="#about">Esci</a></li>
+                   </ul>
+        </div>
+
+
+
+
+    <div class="main">
+        <h2>Responsive Admin page</h2>
+        <p>Resize the browser window to see how it works.</p>   
+         <h4>Hello i'm admin</h4>
+        <h4>Hello i'm admin</h4>
+        <h4>Hello i'm admin</h4>
+    </div>
+
+</body>
+
+</html>
---
+++ html/templates/validator.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Validator;
+
+class Validator{
+        /**
+         * @var array $patterns
+         */
+        public $patterns = array(
+            'uri'           => '[A-Za-z0-9-\/_?&=]+',
+            'url'           => '[A-Za-z0-9-:.\/_?&=#]+',
+            'alpha'         => '[\p{L}]+',
+            'words'         => '[\p{L}\s]+',
+            'alphanum'      => '[\p{L}0-9]+',
+            'int'           => '[0-9]+',
+            'float'         => '[0-9\.,]+',
+            'tel'           => '[0-9+\s()-]+',
+            'text'          => '[\p{L}0-9\s-.,;:!"%&()?+\'°#\/@]+',
+            'file'          => '[\p{L}\s0-9-_!%&()=\[\]#@,.;+]+\.[A-Za-z0-9]{2,4}',
+            'folder'        => '[\p{L}\s0-9-_!%&()=\[\]#@,.;+]+',
+            'address'       => '[\p{L}0-9\s.,()°-]+',
+            'date_dmy'      => '[0-9]{1,2}\-[0-9]{1,2}\-[0-9]{4}',
+            'date_ymd'      => '[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}',
+            'email'         => '[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+[.]+[a-z-A-Z]'
+        );
+
+    public static function emailValidator($email){
+        filter_var($value, FILTER_VALIDATE_EMAIL) ? return true : return false; //  Filters a variable with a specified filter (FILTER_VALIDATE_EMAIL)
+    }
+    public static function passwordValidation($password){
+        $number    = preg_match('@[0-9]@', $password); // must contain at least one number
+        if(!$number  || strlen($password) < 8 || strlen($password) > 16) ? return false: return true; // password lenght must be: 8 <= password.lenght <= 16
+    }
+    public static function nameValidator($name){
+        return preg_match("/^([a-zA-Z' ]+)$/",$name);
+    }
+    function validateDate($date, $format = 'd/m/Y') // change the format parameters if you want to check others date format
+    {
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) == $date;
+    }
+    public static function cfValidator($cf)
+    {
+            if($cf=='')
+           return false;
+       
+            if(strlen($cf)!= 16)
+           return false;
+       
+            $cf=strtoupper($cf);
+            if(!preg_match("/[A-Z0-9]+$/", $cf))
+           return false;
+            $s = 0;
+            for($i=1; $i<=13; $i+=2){
+           $c=$cf[$i];
+           if('0'<=$c and $c<='9')
+                $s+=ord($c)-ord('0');
+           else
+                $s+=ord($c)-ord('A');
+            }
+       
+            for($i=0; $i<=14; $i+=2){
+           $c=$cf[$i];
+            switch($c){
+                    case '0':  $s += 1;  break;
+                    case '1':  $s += 0;  break;
+                    case '2':  $s += 5;  break;
+                    case '3':  $s += 7;  break;
+                    case '4':  $s += 9;  break;
+                    case '5':  $s += 13;  break;
+                    case '6':  $s += 15;  break;
+                    case '7':  $s += 17;  break;
+                    case '8':  $s += 19;  break;
+                    case '9':  $s += 21;  break;
+                    case 'A':  $s += 1;  break;
+                    case 'B':  $s += 0;  break;
+                    case 'C':  $s += 5;  break;
+                    case 'D':  $s += 7;  break;
+                    case 'E':  $s += 9;  break;
+                    case 'F':  $s += 13;  break;
+                    case 'G':  $s += 15;  break;
+                    case 'H':  $s += 17;  break;
+                    case 'I':  $s += 19;  break;
+                    case 'J':  $s += 21;  break;
+                    case 'K':  $s += 2;  break;
+                    case 'L':  $s += 4;  break;
+                    case 'M':  $s += 18;  break;
+                    case 'N':  $s += 20;  break;
+                    case 'O':  $s += 11;  break;
+                    case 'P':  $s += 3;  break;
+                    case 'Q':  $s += 6;  break;
+                    case 'R':  $s += 8;  break;
+                    case 'S':  $s += 12;  break;
+                    case 'T':  $s += 14;  break;
+                    case 'U':  $s += 16;  break;
+                    case 'V':  $s += 10;  break;
+                    case 'W':  $s += 22;  break;
+                    case 'X':  $s += 25;  break;
+                    case 'Y':  $s += 24;  break;
+                    case 'Z':  $s += 23;  break;
+            }
+           }
+       
+           if( chr($s%26+ord('A'))!=$cf[15] )
+           return false;
+       
+           return true;
+       }
+    }
+    public static function validateMobile($mobile)
+    {
+        return preg_match('/^[0-9]{10}+$/', $mobile);
+    }
+    public static function UserValidation()
+    {
+
+    }
+
+}
+?>
