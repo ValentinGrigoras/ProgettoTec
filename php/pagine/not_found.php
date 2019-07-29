217ad31c7648f57058d182107c6662a2379b0668
Irina <irina_030@protonmail.com>
2019-07-29 23:00:06 +0200

    separazione funzionante

--- php/pagine/not_found.php
+++ php/pagine/not_found.php
@@ -1,4 +1,6 @@
 <?php
-$error_notFound = file_get_contents(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "html" . DIRECTORY_SEPARATOR . "pagine" . DIRECTORY_SEPARATOR . "not_found.html");
-echo $error_notFound;
+
+$page = file_get_contents(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "html" . DIRECTORY_SEPARATOR . "pagine" . DIRECTORY_SEPARATOR . "not_found.html");
+$page = str_replace("*linkhome*","./",$page);
+echo $page;
 ?>
