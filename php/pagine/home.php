4bb84e70f2326daf76fb6ec2c5995f06336304af
ValentinGrigoras <ghury1@protonmail.com>
2019-07-29 12:08:18 +0200

    fix

---
+++ php/pagine/home.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "./../../php/database/database.php";
+require_once "./../../php/tools/validator.php";
+
+use Database\Database;
+use Validator\Validator;
+
+$database = new Database();
+
+?>
