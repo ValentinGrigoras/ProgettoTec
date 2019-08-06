2f7a73cf0c4a936198077fd3e1df42c33928f6f3
Irina <irina_030@protonmail.com>
2019-08-06 15:42:52 +0200

    progress css menu

--- php/templates/head.php
+++ php/templates/head.php
@@ -39,4 +39,3 @@ if ($counter > 0) Utilities::checkCounter($counter,$tabIndex);
 $header = str_replace("*linkinfo*","<li><a href='./info-e-contatti' tabindex=\"$tabIndex\">Info e Contatti</a></li>",$header,$counter);*/
 
 echo $head;
-echo 25%6;
