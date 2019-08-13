cbfd839bf08806c2fecbf3381c8657f4073562e1
Irina <irina_030@protonmail.com>
2019-08-13 22:08:17 +0200

    fix form registrazione

--- php/pagine/registrazione.php
+++ php/pagine/registrazione.php
@@ -16,34 +16,34 @@ if ($database) {
 		$error=false;
 		if (!Validator::emailValidator($_POST['email'])){
 			$error=true;
-			$page=str_replace('*erroremail*', 'Il campo <span xml:lang=\"en\">Email</span> inserito non è corretto. Rispettare il formato indicato.', $page);
+			$page=str_replace('<p class="hidden">*erroremail*</p>', '<p class="error">Il campo <span xml:lang=\"en\">Email</span> inserito non è corretto. Rispettare il formato indicato.</p>', $page);
 		}
 		$user = Database::selectUser($_POST['email']);
 		if (empty($user)) {
 			if (!Validator::passwordValidator($_POST['password'])){
 				$error=true;
-				$page=str_replace('*errorepassword*', "La password non rispetta le indicazioni. Rispettare il formato indicato.", $page);
+				$page=str_replace('<p class="hidden">*errorepassword*</p>', '<p class="error">La <span xml:lang=\"en\">password</span> non rispetta le indicazioni. Rispettare il formato indicato.</p>', $page);
 			}
 			if (!Validator::nameValidator($_POST['nome'])){
 				$error=true;
-				$page=str_replace('*errorenome*', "Il nome inserito non &egrave; valido. Rispettare il formato indicato.", $page);
+				$page=str_replace('<p class="hidden">*errorenome*</p>', '<p class="error">Il nome inserito non &egrave; valido. Rispettare il formato indicato.</p>', $page);
 			}
 			if (!Validator::nameValidator($_POST['cognome'])){
 				$error=true;
-				$page=str_replace('*errorecognome*', "Il cognome inserito non &egrave; valido. Rispettare il formato indicato.", $page);
+				$page=str_replace('<p class="hidden">*errorecognome*</p>', '<p class="error">Il cognome inserito non &egrave; valido. Rispettare il formato indicato.</p>', $page);
 			}
 			if (!Validator::cfValidator($_POST['cf'])){
 				$error=true;
-				$page=str_replace('*errorecf*', "Il codice fiscale inserito non &egrave; valido. Rispettare il formato indicato.", $page);
+				$page=str_replace('<p class="hidden">*errorecf*</p>', '<p class="error">Il codice fiscale inserito non &egrave; valido. Rispettare il formato indicato.</p>', $page);
 			}
 			if (!Validator::mobileValidator($_POST['telefono'])){
 				$error=true;
-				$page=str_replace('*erroretelefono*', "Il numero di telefono inserito non &egrave; valido. Rispettare il formato indicato.", $page);
+				$page=str_replace('<p class="hidden">*erroretelefono*</p>', '<p class="error">Il numero di telefono inserito non &egrave; valido. Rispettare il formato indicato.</p>', $page);
 			}
 			$data= date('Y-m-d',strtotime($_POST['datanascita']));
 			if (!Validator::dateValidator($data)){
 				$error=true;
-				$page=str_replace('*erroredatanascita*', "La data di nascita inserita non &egrave; valido. Rispettare il formato indicato.", $page);
+				$page=str_replace('<p class="hidden">*erroredatanascita*</p>', '<p class="error">La data di nascita inserita non &egrave; valido. Rispettare il formato indicato.</p>', $page);
 			}
             if (!$errore){
             	$registeruser = Database::registerUser($_POST['email'], $_POST['password'], $_POST['nome'], $_POST['cognome'], $data, $_POST['cf'], $_POST['telefono']);
