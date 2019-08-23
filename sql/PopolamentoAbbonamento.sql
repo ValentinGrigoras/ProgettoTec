a3f2599c9201b0d3b843ed8d0af1a748abb1bb16
Valentin <ghury1@gmail.com>
2019-08-23 19:22:49 +0200

    prezzi

--- sql/PopolamentoAbbonamento.sql
+++ sql/PopolamentoAbbonamento.sql
@@ -1,4 +1,4 @@
-INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione) VALUES ('1','Mensile','50','');
-INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione) VALUES ('2','Trimestrale','120','');
-INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione) VALUES ('3','Semestrale','250','');
-INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione) VALUES ('4','Annuale','360','');
+INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione,nomeImg) VALUES ('1','Mensile','50','','abb1.png');
+INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione,nomeImg) VALUES ('2','Trimestrale','120','abb2.png');
+INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione,nomeImg) VALUES ('3','Semestrale','250','','abb3.png');
+INSERT INTO Abbonamento (idAbbonamento,tipoAbbonamento,prezzo,descrizione,nomeImg) VALUES ('4','Annuale','360','','abb4.png');
