1536905a0690e9130e9c8aa935cf944f81b90b66
Irina <irina_030@protonmail.com>
2019-09-12 13:54:08 +0200

    m

---
+++ php/pagine/chi_siamo.php
@@ -0,0 +1,36 @@
+<?php 
+$page = file_get_contents(dirname(dirname(__DIR__)) . '/' . "html" . '/' . "pagine" . '/' . "chi_siamo.html");
+
+
+// Directory
+$directory = "../../img/about_us";
+
+// Returns array of files
+$files = scandir($directory);
+unset($files[0]); unset($files[1]); //rimossi '.' e '..'
+$generaimg="";
+// Count number of files and store them to variable..
+$num_files = count($files);
+
+
+		$i=2;
+		while($i<=$num_files+1){
+			$generaimg.="
+				<div class=\"mySlides fade\">";
+			$i-=1;
+			$generaimg.= "
+    				<div class=\"numbertext\">$i / $num_files</div>" ;
+    		$i+=1;
+    		$generaimg.= "
+    				<img src=\"../../img/about_us/$files[$i]\" >
+    				<div class=\"text\">Caption Text</div>
+  				</div>";
+
+  			$i++;
+		}
+
+$page = str_replace("*generaimg*",$generaimg,$page);
+
+
+echo $page;
+?>
