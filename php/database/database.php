93552e604d2f807b82e0cbe2693c0ee9afac42bb
Irina <irina_030@protonmail.com>
2019-09-12 16:58:25 +0200

    fix path

--- php/database/database.php
+++ php/database/database.php
@@ -10,14 +10,7 @@ class Database {
     const PASSWORD = "TecWeb";
     const DB_NAME = "Palestra";
 
-    
-
-    /*
-    const HOST_DB = "localhost";
-    const USERNAME = "root";
-    const PASSWORD = "Chemer9.";
-    const DB_NAME = "Palestra";
-*/
+  
 
     private static $connection;
 
