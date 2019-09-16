a87f25b7bfab05c3a6db67f48bf973d17ee81b6d
Irina <irina_030@protonmail.com>
2019-09-16 21:49:41 +0200

    fix

--- php/pagine/user_abb.php
+++ php/pagine/user_abb.php
@@ -20,30 +20,29 @@ $errore = false;
             $corsi="";
             $allcourses = Database::selectCourses();
             $UserCourses = Database::getCoursesByUserID( $_SESSION['id']);
+            var_dump($UserCourses);
             $page = str_replace('*formCorsi', '<form id="modifyCourses_form" action="./user_abb" method="POST" >
                     <fieldset *stato*>
                         <div class="form_entry">
-                            <div class="required">
-                                <label for="email">Seleziona corso</label>
-                            </div>
-                            <div class="input_feedb">
-                                <select name="corsoSelezionato">
+                            <label for="corsoSelezionato">Seleziona corso</label>
+                            <select name="corsoSelezionato">
                                    *corsi*
-                                </select>
-                            </div>
+                            </select>
+                            
                         </div>
                     </fieldset>
                     <button id="salvaCorso" type="submit" name="salvaCorso" value="salvaCorso">Aggiungi corso</button>
-                    <p class="hidden">*confirmmessage*</p></form>', $page);
-        if(count($UserCourses) >1){
-            $page = str_replace('*stato*', 'disabled id="disabledField"', $page);
-            $page = str_replace('*titoloinfo2*', "<h2>Attenzione: Hai già scelto il massimo dei corsi offerti dalla palestra! Aspetta la fine dell'abbonamento</h2>", $page);
-        }
-        else{
-            $page = str_replace('*stato*', '', $page);
-            $page = str_replace('*titoloinfo2*', "", $page);
-            $page=str_replace('*confirmmessage*', '', $page);
-        } 
+                    *confirmmessage*', $page);
+
+            if(count($UserCourses) >1){
+                $page = str_replace('*stato*', 'disabled id="disabledField"', $page);
+                $page = str_replace('*titoloinfo2*', "<h2>Attenzione: Hai già scelto il massimo dei corsi offerti dalla palestra! Aspetta la fine dell'abbonamento</h2>", $page);
+            }
+            else{
+                $page = str_replace('*stato*', '', $page);
+                $page = str_replace('*titoloinfo2*', "", $page);
+                $page=str_replace('*confirmmessage*', '', $page);
+            } 
             for ($i=0; $i < count($allcourses) ; $i++) {
                 for ($j=0; $j < count($UserCourses); $j++) { 
                     if($allcourses[$i]['nome'] === $UserCourses[$j]['nome'])
@@ -51,27 +50,25 @@ $errore = false;
                             $i++; $j=0;
                         } 
                 }
-            $corsi .= '<option value="'.$allcourses[$i]['nome'].'">'.$allcourses[$i]['nome'].'</option>';
+                $corsi .= '<option value="'.$allcourses[$i]['nome'].'">'.$allcourses[$i]['nome'].'</option>';
             }
-        }
-        else{ // se l'utente non ha ancora un abbonamento
+        }else{ // se l'utente non ha ancora un abbonamento
             $page = str_replace('*titoloinfo2*', "<p>Non hai nessun abbonamento attivo! Scegliene uno:<p>", $page);
-             $page = str_replace('*formCorsi*', '', $page);
-             $page = str_replace('*formAbb*', '<form id="AddAbb_form" action="./user_abb" method="POST" >
+            $page = str_replace('*formCorsi*', '', $page);
+            $page = str_replace('*formAbb*', '<form id="AddAbb_form" action="./user_abb" method="POST" >
                     <fieldset *stato*>
                         <div class="form_entry">
-                            <div class="required">
-                                <label for="abbonamento">Seleziona abbonamento</label>
-                            </div>
-                            <div class="input_feedb">
+                            <label for="abbonamento">Seleziona abbonamento</label>
+                            
+                            
                                 <select name="abbSelezionato">
                                    *abbSelect*
                                 </select>
-                            </div>
+                            
                         </div>
                     </fieldset>
                     <button id="salvaAbb" type="submit" name="salvaAbb" value="salvaAbb">Conferma abbonamento</button>
-                    <p class="hidden">*confirmmessage*</p></form>', $page);
+                    *confirmmessage*', $page);
 
              $abbonamenti = Database::getSubscriptionsTypes();
              for ($i=0; $i < count($abbonamenti) ; $i++) {
@@ -80,20 +77,12 @@ $errore = false;
             $page = str_replace("abbSelect", $abbon, $page);
 
             
-              if (isset($_POST['salvaAbb'])){ //non è stato fatto submit
-                $sub = Database::getSubscriptionsTypes($_POST['abbSelezionato']);
-                //
-            var_dump($_SESSION['id']);
-            echo Date("Y-m-d",strtotime("now + 1 month"));
-            var_dump($sub[0]['idAbbonamento']);
-            $today = date("Y-m-d");
-$tomorrow = Validator::validateSubscriptionDate($sub[0]['tipoAbbonamento']);
-                echo $today;
-                echo Validator::validateSubscriptionDate($sub[0]['tipoAbbonamento']);
-
-                    $esito = Database::InsertUserSubscription($_SESSION['id'], $sub[0]['idAbbonamento'],date("Y-m-d"),Validator::validateSubscriptionDate($sub[0]['tipoAbbonamento']));
+            if (isset($_POST['salvaAbb'])){ // è stato fatto submit
+                $sub = Database::getSubscriptionsId($_POST['abbSelezionato']);
+            
+                $esito = Database::InsertUserSubscription($_SESSION['id'], 1 ,date("Y-m-d"),Validator::validateSubscriptionDate($sub[0]['tipoAbbonamento']););
                     var_dump($esito);
-               }
+            }
                
         }   
        
