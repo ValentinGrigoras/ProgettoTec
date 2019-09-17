7050b34b627ebe957f7134d094e8291c6675434c
Valentin <ghury1@gmail.com>
2019-09-17 02:34:33 +0200

    fix sql img

--- sql/PopolamentoAbbonamento.sql
+++ sql/PopolamentoAbbonamento.sql
@@ -1,4 +1,4 @@
 INSERT INTO `Abbonamento` (`idAbbonamento`, `tipoAbbonamento`, `prezzo`, `descrizione`, `nomeImg`) VALUES
-(1, 'Mensile', '49.99', 'Ci sono tante attività da abbinare.Ogni attività ha un costo fisso da aggiungere al costo base dell\'abbonamento.', 'abb1.jpg'),
-(2, 'Trimestrale', '139.99', 'Ci sono tante attività da abbinare.Ogni attività ha un costo fisso da aggiungere al costo base dell\'abbonamento.', 'abb2.jpg'),
-(4, 'Annuale', '429.99', 'Ci sono tante attività da abbinare.Ogni attività ha un costo fisso da aggiungere al costo base dell\'abbonamento.', 'abb3.jpg');
+(1, 'Mensile', '49.99', 'Ci sono tante attività da abbinare.Ogni attività ha un costo fisso da aggiungere al costo base dell\'abbonamento.', 'one.png'),
+(2, 'Trimestrale', '139.99', 'Ci sono tante attività da abbinare.Ogni attività ha un costo fisso da aggiungere al costo base dell\'abbonamento.', 'three.png'),
+(4, 'Annuale', '429.99', 'Ci sono tante attività da abbinare.Ogni attività ha un costo fisso da aggiungere al costo base dell\'abbonamento.', 'twelve.png');
